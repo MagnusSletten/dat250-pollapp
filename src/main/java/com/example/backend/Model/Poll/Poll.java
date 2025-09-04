061310cd7ceb5a3fde229326ec6bca9c271d424b
MagnusSletten <magnus.elias.sletten@gmail.com>
2025-09-04 11:06:26 +0200

    Add timestamps to poll objects

--- src/main/java/com/example/backend/Model/Poll/Poll.java
+++ src/main/java/com/example/backend/Model/Poll/Poll.java
@@ -1,11 +1,17 @@
 package com.example.backend.Model.Poll;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import lombok.Data;
 
 @Data
 public class Poll {
     private Integer pollID; 
     private String title; 
-    private PollQuestion question; 
+    private PollQuestion question;
+    private Instant publishedAt = Instant.now(); 
+    private Instant validUntil = Instant.now().plus(7, ChronoUnit.DAYS);
+
     
 }
