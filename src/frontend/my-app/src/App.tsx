69bb1836ea7e3c2cc7aca8b4abd6eae3fd7c0404
Georg Risøy <georg.mri@gmail.com>
2025-11-03 19:50:03 +0100

    front-end oppdatering

--- src/frontend/my-app/src/App.tsx
+++ src/frontend/my-app/src/App.tsx
@@ -5,50 +5,58 @@ import LogInComponent from "./Components/LogInComponent";
 import CreateUserComponent from "./Components/CreateUserComponent";
 
 export default function App() {
-  const [pageState, setPageState] = useState(0);
-  const [loginStatus, setLoginStatus] = useState(false);
-  const [userName, setUsername] = useState("");
+    const [pageState, setPageState] = useState(0);
+    const [loginStatus, setLoginStatus] = useState(false);
+    const [userName, setUsername] = useState("");
 
-return (
-  <>
-  <div className="Title">
-    <h1>Welcome to our polling app</h1>
-  </div>
-  <div className="app-container">
- 
-    <div className="topbar">
-      <div className="login-box">
-        <button className="page-state-button" onClick={() => setPageState(0)}>
-        { loginStatus ? "User profile" : "Log in"}
-      </button>
-      </div>
-    </div>
+    return (
+        <>
+            <div className="top-utility-bar">
+                <div className="auth-actions">
+                    <button
+                        className="btn-utility"
+                        onClick={() => setPageState(0)}
+                    >
+                        { loginStatus ? "User profile" : "Log in"}
+                    </button>
 
-    <div className="page-tabs">
-      <button className="page-state-button" onClick={() => setPageState(1)}>Poll-Creation</button>
-      <button className="page-state-button" onClick={() => setPageState(2)}>Vote</button>
-      <button className="page-state-button" onClick={() => setPageState(3)}>Create User</button>
-    </div>
+                    <button
+                        className="btn-utility"
+                        onClick={() => setPageState(3)}
+                    >
+                        Create User
+                    </button>
+                </div>
+            </div>
 
-    <div className="content">
-      {pageState === 1 ? (
-        <CreatePollComponent userName={userName} loginStatus={loginStatus} />
-      ) : pageState === 2 ? (
-        <VotePollComponent userName={userName} />
-      ) : pageState === 0 ? (
-        <LogInComponent
-          userName={userName}
-          setLoginStatus={setLoginStatus}
-          setUsername={setUsername}
-          loginStatus={loginStatus}
-        />
-      ) : (
-        <CreateUserComponent />
-      )}
-    </div>
+            <div className="Title">
+                <h1>Welcome to our polling app</h1>
+            </div>
+            <div className="app-container">
 
-    {loginStatus && <h2 className="login-status">Logged in as {userName}</h2>}
-  </div>
-  </>
-);
+                <div className="page-tabs">
+                    <button className="page-state-button" onClick={() => setPageState(1)}>Poll-Creation</button>
+                    <button className="page-state-button" onClick={() => setPageState(2)}>Vote</button>
+                </div>
+
+                <div className="content">
+                    {pageState === 1 ? (
+                        <CreatePollComponent userName={userName} loginStatus={loginStatus} />
+                    ) : pageState === 2 ? (
+                        <VotePollComponent userName={userName} />
+                    ) : pageState === 0 ? (
+                        <LogInComponent
+                            userName={userName}
+                            setLoginStatus={setLoginStatus}
+                            setUsername={setUsername}
+                            loginStatus={loginStatus}
+                        />
+                    ) : (
+                        <CreateUserComponent />
+                    )}
+                </div>
+                {loginStatus && <h2 className="login-status">Logged in as {userName}</h2>}
+            </div>
+        </>
+    );
 }
